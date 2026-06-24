3ccf4dfaf4eb7babe5f5ce79df8f7da7d23f9a88
Jim Rice <ricejpreston@gmail.com>
2026-06-24 11:34:08 -0700

    Auto-commit: src/main/java/com/eventmanager/model/Event.java

--- src/main/java/com/eventmanager/model/Event.java
+++ src/main/java/com/eventmanager/model/Event.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
